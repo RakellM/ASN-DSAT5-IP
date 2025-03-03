2f4797cd38bf6ae4b1a6f6fcd2b822d604bd68ec
Rakell <raquelsmarques@gmail.com>
2025-03-02 21:02:50 -0500

    fest: Conditional examples.

---
+++ class02-20250226/03_drinking.py
@@ -0,0 +1,24 @@
+# %%
+age = 76
+name = "Anna Presley"
+
+if age > 75:
+    print("Drink responsibly and slowly.")
+    
+    if "Presley" in name:
+        print("I am a fan of Elvis Presley.")
+
+elif age > 60:
+    print("Drink responsibly.")
+
+elif age >= 18:
+    print("Go ahead and drink. Get wasted!")
+    print("Just don't drive.")
+
+else:
+    print("Go home and drink milk, kid.")
+    print("Your mom is waiting for you.")
+
+
+print("End of script")
+

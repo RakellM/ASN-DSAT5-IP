e86bea148787b481c0609178a57b3cd68a3b018d
Rakell <raquelsmarques@gmail.com>
2025-03-02 20:03:57 -0500

    feat: Boolean explanation with Truth Table

---
+++ class02-20250226/02_bool.py
@@ -0,0 +1,70 @@
+# %%
+name = "Raquel"
+print(type(name))
+
+# %% 
+print(True)
+print(False)
+
+print(type(True))
+print(type(1))
+print(type(1.))
+
+# %%
+print(int(False))
+print(int(True))
+
+print(bool(10))
+print(bool(0))
+print(bool(-1))
+
+# %%
+print(10 > 5)
+print( 10 < 5)
+
+print( "apple" in "pineapple")
+print( "apple" in "watermelon")
+
+# %%
+# Truth table for AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print("True and True: ", True and True)
+print("True and False: ", True and False)
+print("False and True: ", False and True)
+print("False and False: ", False and False)
+
+print("True * True: ", True * True)
+print("True * False: ", True * False)
+print("False * True: ", False * True)
+print("False * False: ", False * False)
+
+print("1 * 1: ", 1 * 1)
+print("1 * 0: ", 1 * 0)
+print("0 * 1: ", 0 * 1)
+print("0 * 0: ", 0 * 0)
+
+# %%
+# Truth table for OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("True or True: ", True or True)
+print("True or False: ", True or False)
+print("False or True: ", False or True)
+print("False or False: ", False or False)
+
+print("True + True: ", True + True)
+print("True + False: ", True + False)
+print("False + True: ", False + True)
+print("False + False: ", False + False)
+
+print("1 + 1: ", 1 + 1)
+print("1 + 0: ", 1 + 0)
+print("0 + 1: ", 0 + 1)
+print("0 + 0: ", 0 + 0)

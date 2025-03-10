c50b0b281db280b4669614e3ee444bf259a8a8e8
Rakell <raquelsmarques@gmail.com>
2025-03-10 15:02:17 -0400

    feat: Add homework P2-02.

---
+++ homework/ex_P2-02.py
@@ -0,0 +1,14 @@
+# 2.2 Write a program that receives a number. Check if the given number is even or odd. Display the result as follows:
+# ` "The number x is odd." `
+# ` "The number x is even." `
+
+# %%
+
+number = int(float(input("Enter a number: ")))
+
+# TODO: fix decimals or message that converts to integer?
+
+if number % 2 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")

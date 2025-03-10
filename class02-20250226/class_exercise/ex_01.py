1a8fc9b14e627a8ca81140b3a4a2f2aff642a142
Rakell <raquelsmarques@gmail.com>
2025-03-10 18:32:59 -0400

    feat: Exercise correction, change code with addition of another option.

--- class02-20250226/class_exercise/ex_01.py
+++ class02-20250226/class_exercise/ex_01.py
@@ -1,29 +1,31 @@
 # Create a program that sells a bottle of water:
 # - If the customer chooses natural mineral water, it will be charged R$1.50
 # - If the customer chooses mineral water with gas, it will be charged R$2.50
+# - If the customer chooses mineral water with lemon, it will be charged R$3.00
 
 # %%
 option_type = input("Choose the type of water you want to buy: \n"
                 "1 - Natural mineral water\n"
-                "2 - Mineral water with gas\n")
-
-option_qty = int(float(input("How many bottles of water do you want to buy?")))
+                "2 - Mineral water with gas\n"
+                "3 - Mineral water with lemon\n")
 
 price = 0
+
+option_qty = int(float(input("How many bottles of water do you want to buy?")))
 
 if option_type == "1":
     price = 1.50
 elif option_type == "2":
     price = 2.50
-else:
-    print("Invalid option, please.\n"
-        "Choose 1 (Natural mineral water) or 2 (Mineral water with gas)")
+elif option_type == "3":
+    price = 3.00
 
-if option_qty < 1:
-    print("Invalid quantity. Please, choose a positive number.")
-else: 
+if price > 0 and option_qty > 0:
     total = price * option_qty
     print(f"Your total to pay is R${total:.2f}")
+else:
+    print("Invalid option.")
+    
 
 
 

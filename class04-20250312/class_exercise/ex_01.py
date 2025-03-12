b73a6e29643e8b57b31a8658a992754232dab1e4
Rakell <raquelsmarques@gmail.com>
2025-03-12 19:18:16 -0400

    feat: Correction of class exercise

--- class04-20250312/class_exercise/ex_01.py
+++ class04-20250312/class_exercise/ex_01.py
@@ -15,6 +15,11 @@ name_last = data[1]
 
 print(f"First Name: {name_first}\nLast Name: {name_last}")
 
+# Correction
+#-----------
+name_first_last = " ".join(data[0:2])
+print(name_first_last)
+
 # %%
 
 # What was the first company he/she worked for?
@@ -22,6 +27,10 @@ company = data[4]
 company_first = data[4][0]
 
 print(f"First company they have worked for: {company_first}")
+
+# Correction
+#-----------
+data[-2][0]
 
 # %%
 
@@ -31,13 +40,30 @@ position_last2 = data[3][-2:]
 
 print(f"Last 2 positions in the market: {position_last2}")
 
+# Correction
+#-----------
+positions = " / ".join(data[-3][-2:])
+
+print(positions)
+
 # %%
 
 # Show the jumps from 2 to 2 companies of this person.
 print(f"Jumps from 2 to 2 companies: {data[4][::2]}")
 
+# Correction
+#-----------
+jumps = " => ".join(data[-2][::2])
+print(jumps)
 
 # %%
 
 # Show the salary from the last to the first
 print(f"Salary from the last to the first: {data[5][::-1]}")
+
+# Correction
+#-----------
+print(data[-1][::-1])
+
+
+

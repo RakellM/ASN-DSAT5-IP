21a1a1a360a2f1c2578d85acdc7ffecbb559ba2b
Rakell <raquelsmarques@gmail.com>
2025-03-31 17:59:54 -0400

    feat: update homework P2-06.

--- python-base/homework/ex_P2-06.py
+++ python-base/homework/ex_P2-06.py
@@ -6,3 +6,10 @@ string1 = input("Enter the first string: ")
 string2 = input("Enter the second string: ")
 
 print(f"{string1} {string2}")
+
+full_string = string1 + " " + string2
+print(full_string)
+
+
+
+# %%
